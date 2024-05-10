691ac77c96c92258c4764af3804f281e439b5851
Uttam Kumar <uttammaurya377@gmail.com>
2024-05-10 21:26:37 +0530

    Nodejs File Handling

--- file.js
+++ file.js
@@ -4,10 +4,20 @@ const fs=require('fs');
 //sync was synchronous calll fs.writeFileSync is create file
 //fs.writeFileSync('./test.txt','Hey There');
 
-
-
-//async 
-//fs.writeFileSync('./test.txt','Hey There Async',(err)=>{})
-
 const res =fs.readFileSync("./test.txt",'utf-8');
 console.log(res);
+
+//async 
+fs.writeFileSync('./test.txt','Hey There Async',(err)=>{
+    if(err) {
+        console.log("Error",err);
+    }else{
+        console.log(res)
+    }
+    }
+)
+
+fs.appendFileSync("./test.txt",new Date().getDate().toLocaleString());
+
+
+console.log(fs.statSync("./test.txt"));
