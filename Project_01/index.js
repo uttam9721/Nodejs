0278debe6e9e72e57b8b20ba75dff018150ec5e6
Uttam Kumar <uttammaurya377@gmail.com>
2024-05-14 22:12:01 +0530

    CRUD Operation in Nodejs

--- Project_01/index.js
+++ Project_01/index.js
@@ -1,13 +1,48 @@
 const express= require('express');
 const fs= require('fs');
 const app = express();
+const mongoose = require('mongoose');
 
-const users = require("./dataset.json");
-const PORT =8000;
+//const users = require("./dataset.json");
+const PORT =8080;
 
 
-// middleware 
-app.use(express.urlencoded({entended: false}));
+//connection 
+
+mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
+.then(()=>console.log('connection connected'))
+.catch(err=>console.log('connection error',err));
+
+
+//Schema -------------------------------------
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type :String ,
+    },
+    email:{
+        type :String,
+        required: true,
+        unique: true,
+    },
+    jobTitle:{
+        type :String,
+
+    },
+    gender:{
+        type :String,
+    },
+},{timestamps:true})
+
+const User =mongoose.model('user',userSchema);
+
+//Middleware - Plugin
+
+ 
+app.use(express.urlencoded({extended: false}));
 app.use((req,res,next)=>{
     console.log("Hello from middleware 1");
     next();
@@ -19,45 +54,67 @@ app.use((req,res,next)=>{
 })
 
 //Routes
+app.get("/ ",async(req,res)=>{
+    const allDbUsers = await Users.find({});
+    const html=`
+    <ul>
+    ${allDbUsers.map((user)=>`<li>${user.firstName}-${user.email}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
 
-app.get("/api/users",(req,res)=>{
-    res.setHeader('x-myname',"uttammaurya"); //custom header 
-    //Always add x to custom headers
-    return res.json(users);
+app.get("/api/users",async(req,res)=>{
+    const allDbUsers = await Users.find({});
+    return res.json(allDbUsers);
 })
 
 app.get('/api/users',(req,res)=>{
     res.json(users);
 })
 
-app.get('/api/users/:id',(req,res)=>{
-     const id = Number(req.params.id);
-     const user = users.find((user)=>user.id===id);
+app.get('/api/users/:id',async(req,res)=>{
+     const user = User.findById(req.params.id);
      if(!user) return res.status(404).json({error:"user not found"});
      return res.json(user);
 })
 
 
-app.post("/api/users",(req,res)=>{
+app.post("/api/users",async(req,res)=>{
     //todo:create new user 
     const body = req.body;
-    if(!body||body.first_name|| !body.last_name|| !body.email || !body.gender || !body.job_title){
+    if(
+        !body||
+        !body.first_name||
+        !body.last_name||
+        !body.email ||
+        !body.gender ||
+        !body.job_title
+    ){
         return res.status(400).json({msg:"All field are req.... "})
     }
-    users.push({...body,id:users.length+1});
-    fs.writeFileSync("./dataset.json",JSON.stringify(users),(err,data)=>{
-       return res.status(201).json({status:"success",id:users.length+1});
-    })
-    return res.json({status:"pending"});
+
+    // it create the user
+const result =   await User.create({
+    firstName : body.first_name,
+    lastName : body.last_name,
+    email : body.email,
+    gender : body.gender ,
+    jobTitle : body.job_title,
+   })
+  // console.log(result);
+    return res.status(201).json({msg:"success"}); 
 });
-app.patch("/api/users/:id",(req,res)=>{
+app.patch("/api/users/:id",async(req,res)=>{
     //todo :edit the user with id 
-    return res.json({status:"pending"});
+    await User.findByIdAndUpdate(req.params.id,{lastName:"Changed"});
+    return res.json({status:"success"});
 })
 
-app.delete("/api/users/:id",(req,res)=>{
+app.delete("/api/users/:id",async(req,res)=>{
     //todo :delete the user with id 
-    return res.json({status:"pending"});
+    await User.findByIdAndDelete(req.param.id);
+    return res.json({status:"success"});
 })
 
 app.listen(PORT,()=>console.log(`Server started at port ${PORT}`));
